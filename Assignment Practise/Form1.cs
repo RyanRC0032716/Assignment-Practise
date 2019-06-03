b4b9545f97ff283f622e08777cf751e4c6cc09f5
rc0032716 <rc0032716@CRO-24-01>
2019-06-03 15:05:01 +0100

    added in code and tab control

--- Assignment Practise/Form1.cs
+++ Assignment Practise/Form1.cs
@@ -16,5 +16,40 @@ namespace Assignment_Practise
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Calculate the area of the rectangle
+            try
+            {
+                double width, height, area;
+                width = Convert.ToDouble(textBox1.Text);
+                height = Convert.ToDouble(textBox2.Text);
+                area = width * height;
+                label3.Text = "Area = " + area;
+            }
+            catch
+            {
+                MessageBox.Show("Error you dummy - Type in two numbers.");
+            }
+ 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Calculate the area of the Triangle
+            try
+            {
+                double width, height, area;
+                width = Convert.ToDouble(textBox3.Text);
+                height = Convert.ToDouble(textBox4.Text);
+                area = width * height / 2;
+                label6.Text = "Area = " + area;
+            }
+            catch
+            {
+                MessageBox.Show("Error your an idiot - Type in two numbers.");
+            }
+        }
     }
 }
